4dff1c8bef90c8fa06f076cc5f859e7d7e6fabc9
its-czarina <czarinacahutay@gmail.com>
2017-03-11 16:38:20 +0800

    create login page

---
+++ application/views/header.php
@@ -0,0 +1,18 @@
+<html>
+	<head>
+		<link rel="stylesheet" href="<?php echo(base_url("application/styles/base.css")) ?>">
+		<link rel="stylesheet" href="<?php echo(base_url("application/styles/header.css")) ?>">
+		<link href="https://fonts.googleapis.com/css?family=Montserrat:300" rel="stylesheet">
+	</head>
+	<body>
+		<div class="title">
+			<div class="navitem">
+				<img id="logo" src="<?php echo(base_url("application/images/logo-1.png")) ?>">
+			</div>
+			<div class="name">
+				<h1>AIDEN</h1>
+				<i>Artificial Intelligence Designed for Educational Needs</i>
+			</div>
+		</div>
+	</body>
+</html>
---
+++ application/views/homepage.php
@@ -0,0 +1,30 @@
+<html>
+	<head>
+		
+	</head>
+	<body>
+		<div id="body">
+			<div class="about-us">
+				<p style="font-size:40px;margin-bottom:0px"> AIDEN, </p><br>
+				<p>Artificial Intelligence Dedicated for Educational Needs, <br><br>
+				is the teacher’s <span style="font-size:30px;font-weight:bolder">best</span> wingman.</p>
+			</div>
+			<div class="login-page">
+			  <div class="form">
+			    <form class="register-form">
+			      <input type="text" placeholder="name"/>
+			      <input type="password" placeholder="password"/>
+			      <input type="text" placeholder="email address"/>
+			      <button>create</button>
+			      <p class="message">Already registered? <a href="#">Sign In</a></p>
+			    </form>
+			    <form class="login-form">
+			      <input type="text" placeholder="Username"/>
+			      <input type="password" placeholder="Password"/>
+			      <button>login</button>
+			    </form>
+			  </div>
+			</div>
+		</div>
+	</body>
+</html>
